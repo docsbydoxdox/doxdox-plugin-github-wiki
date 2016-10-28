4ac5c2d4f936ed6ca5b82c555ff93b6d7e497b6b
Scott Doxey <neogeek0101@gmail.com>
2016-10-28 03:12:28 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# doxdox-plugin-github-wiki
+
+> GitHub wiki export plugin for doxdox.
+
+[![NPM Version](http://img.shields.io/npm/v/doxdox-plugin-github-wiki.svg?style=flat)](https://www.npmjs.org/package/doxdox-plugin-github-wiki)
+
+## Install
+
+```bash
+$ npm install doxdox doxdox-plugin-github-wiki --save-dev
+```
+
+## Usage
+
+```bash
+$ doxdox 'src/**/*.js' --layout github-wiki --output wiki.zip
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const Handlebars = require('handlebars');
+
+const admzip = require('adm-zip');
+
+const plugin = data => new Promise((resolve, reject) => {
+
+    const zip = new admzip();
+
+    fs.readFile(path.join(__dirname, 'template.hbs'), 'utf8', (err, contents) => {
+
+        if (err) {
+
+            return reject(err);
+
+        }
+
+        const template = Handlebars.compile(contents);
+
+        data.files.forEach(file => {
+
+            file.methods.forEach(method => {
+
+                zip.addFile(
+                    `${file.name}/${method.name}.md`,
+                    template(method)
+                );
+
+            });
+
+        });
+
+        return resolve(zip.toBuffer());
+
+    });
+
+});
+
+module.exports = plugin;
